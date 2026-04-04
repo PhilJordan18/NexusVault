4482e312f4a9b29897512258d0b149980b991a40
Philippe Mba <158060610+PhilJordan18@users.noreply.github.com>
2026-04-03 20:26:23 -0400

    chore: set database'scheme

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,55 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('salt', 64);
+            $table->text('public_key');
+            $table->text('private_key');
+            $table->boolean('mfa_enabled')->default(false);
+            $table->text('totp_secret');
             $table->rememberToken();
+            $table->timestamps();
+        });
+
+
+        Schema::create('services', function (Blueprint $table) {
+           $table->id();
+           $table->foreignId('user_id')->constrained()->onDelete('cascade');
+           $table->string('name');
+           $table->string('url')->nullable();
+           $table->string('favicon')->nullable();
+
+           $table->string('username');
+           $table->string('password');
+           $table->string('notes');
+
+           $table->string('iv', 64);
+           $table->string('tag', 64);
+
+           $table->foreignId('shared_user_id')->nullable()->constrained('users');
+           $table->timestamp('shared_at')->nullable();
+           $table->timestamps();
+        });
+
+        Schema::create('shares', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id')->constrained()->onDelete('cascade');
+            $table->foreignId('from_user_id')->constrained('users');
+            $table->foreignId('to_user_id')->constrained('users');
+            $table->timestamp('shared_at')->useCurrent();
+            $table->timestamp('accepted_at')->nullable();
+            $table->boolean('rejected')->default(false);
+            $table->text('shared_data');
+            $table->timestamps();
+        });
+
+        Schema::create('passkeys', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('credential_id');
+            $table->text('public_key');
+            $table->string('user_handle');
+            $table->timestamp('last_used_at');
             $table->timestamps();
         });
 
@@ -42,6 +90,9 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('passkeys');
+        Schema::dropIfExists('shares');
+        Schema::dropIfExists('services');
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
